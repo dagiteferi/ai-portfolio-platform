583cf9f1ec3de1ac4e1617b3167bca77429392cd
dagiteferi <dagiteferi2011@gmail.com>
2025-07-03 10:18:01 +0300

    add footer page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navigation from'./components/Navigation';
 import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Education from './components/Education';
 import Projects from './components/Projects';
 import Work from './components/Work';
-import Education from './components/Education';
 import Contact from './components/Contact';
-import About from './components/About' 
-import Services from './components/Services';
+import Footer from './components/Footer'; 
+
+
 
 function App() {
   return (
@@ -15,10 +18,12 @@ function App() {
       <Hero />
       <About />
       <Services />
-      <Projects />
-      <Work />
       <Education />
+      <Work />
+      <Projects />
       <Contact />
+      <Footer />
+      
     </div>
   );
 }
