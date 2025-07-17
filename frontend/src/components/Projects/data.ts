c7864521d45c723b0307bdc22d027d30fab62be8
dagiteferi <dagiteferi2011@gmail.com>
2025-07-17 17:54:34 +0300

    fix the link issue

--- frontend/src/components/Projects/data.ts
+++ frontend/src/components/Projects/data.ts
@@ -7,7 +7,7 @@ export const projects = [
     description: 'Advanced prompt engineering framework for optimizing LLM outputs with visual mapping of prompt components to techniques.',
     
     technologies: ['Python', 'Gemini API', 'LangChain', 'React', 'fastapi' , 'vector Db' ,'redis'],
-    image:'../assets/ai portifilo.png',
+    image:'/assets/ai portifilo.png',
     github: 'https://github.com/dagiteferi/ai-portfolio-platform',
     demo: 'https://prompt-optimizer-demo.com',
     featured: true
@@ -18,8 +18,8 @@ export const projects = [
     description: 'My final year project under development.',
     
     technologies: ['Python', 'React', 'FastApi', 'PostgrewSql'],
-    image:'../assets/rent managent .png',
-    github: 'hhttps://github.com/dagiteferi/RENT-MANAGMENT-SYSTEM',
+    image:'/assets/rent managent .png',
+    github: 'https://github.com/dagiteferi/RENT-MANAGMENT-SYSTEM',
     demo: '#'
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     description: 'leveraging data from an eCommerce platform to enable a buy-now-pay-later service for customers.',
     
     technologies: ['Python', 'Scikit-learn', 'mlops', 'FastAPI'],
-    image:'../assets/Credit Risk Analyzer.png',
+    image:'/assets/Credit Risk Analyzer.png',
     github: 'https://github.com/dagiteferi/Credit-scoring-model',
     demo: 'https://credit-scoring-frontend.onrender.com/'
   },
@@ -38,7 +38,7 @@ export const projects = [
     description: ' predict stock prices (TSLA, BND, SPY) using ARIMA, SARIMA, and LSTM models. It optimizes portfolios to maximize returns, minimize risks, and provides actionable insights for data-driven investment decisions.',
     
     technologies: ['ARIMA', 'SARIMA', 'LSTM', 'python'],
-    image:'../assets/time seri.png',
+    image:'/assets/time seri.png',
     github: 'https://github.com/dagiteferi/TimeSeries-Portfolio-Optimization',
     demo: '#'
   },
@@ -48,7 +48,7 @@ export const projects = [
     description: 'to analyze Brent oil price fluctuations by detecting change points and identifying their causes using statistical modeling. This includes Bayesian methods, ARIMA, GARCH, and interactive visualizations to provide insights for investors, policymakers, and analysts.',
     
     technologies: ['GARCH', 'ARIMA', 'python'],
-    Image: '../assets/Brent Oil.png',
+    image: '/assets/Brent Oil.png',
     github: 'https://github.com/dagiteferi/brent-price-change-analysis',
     demo: '#'
   },
@@ -67,7 +67,7 @@ export const projects = [
     description: ' develop the detection of fraudulent transactions in e-commerce using advanced machine learning techniques.It involves data analysis, preprocessing, feature engineering, model building, and deployment.The project includes API with Flask, containerization with Docker, and dashboards with Dash ',
     
     technologies: ['Python', 'LIME', 'Dash', 'Flask-Api', 'Docker'],
-    image: '../assets/fround detaction.png',
+    image:'/assets/fround detaction.png',
     github: 'https://github.com/dagiteferi/fraud-detection-models',
     demo: '#'
   },
@@ -77,7 +77,7 @@ export const projects = [
     description: 'data warehouse for Ethiopian medical businesses by scraping data from Telegram channels. The project integrates object detection using YOLO and provides CRUD operations via FastAPI to enable comprehensive data analysis and user interaction.',
     
     technologies: ['Python', 'postgresql', 'Yolo', 'Dpt' , 'FastAPI', 'Scrapy'],
-    Image: '../assets/medical data .png',
+    image: '/assets/medical data .png',
     github: 'https://github.com/dagiteferi/Ethiopian-Medical-DataWarehouse',
     demo: '#'
   },
@@ -87,7 +87,7 @@ export const projects = [
     description: 'fine-tuning Named Entity Recognition (NER) models for the Amharic language, aimed at extracting key entities such as product names, prices, and locations from Ethiopian-based e-commerce Telegram channels.Dataset Source: Telegram e-commerce channels (e.g. @mertteka)',
     
     technologies: ['Python', 'NLP', 'NER', 'amharic languch '],
-    image: '../assets/LLM.png',
+    image: '/assets/LLM.png',
     github: 'https://github.com/dagiteferi/EthioMart-Amharic-NERLLM-Model',
     demo: '#'
   },
@@ -97,7 +97,7 @@ export const projects = [
     description: 'analysis of TellCo, focusing on user behavior, engagement, experience, and satisfaction. Includes data preparation, exploratory analysis, dashboard development, and predictive modeling to provide actionable insights for growth opportunities.',
     
     technologies: ['Python', 'behavior-analysis', 'sfaction-analysis', 'Notebook'],
-    image:'../assets/useroverview.png',
+    image:'/assets/useroverview.png',
     github: 'https://github.com/dagiteferi/User-Overview-Engagement-and-Experience-Analysis',
     demo: '#'
   },
@@ -107,7 +107,7 @@ export const projects = [
     description: 'An end-to-end machine learning solution for forecasting sales across all stores of Rossmann Pharmaceuticals in several cities, six weeks ahead. Accurate sales predictions will assist the finance team in better planning and decision-making',
     
     technologies: ['Python', 'flask', 'ML', 'Xgboost', 'sales prediction'],
-    image:'../assets/salxe prediction.png',
+    image:'/assets/salxe prediction.png',
     github: 'https://github.com/dagiteferi/sales-Prediction-model',
     demo: 'https://sales-prediction-model.vercel.app'
   },
@@ -117,7 +117,7 @@ export const projects = [
     description: 'Spiritual Tracker App for Ethiopian Gospel Believers Church Overview The Spiritual Tracker App supports the spiritual journey of the Ethiopian Gospel Believers Church community. It provides daily Bible verses and devotions, a platform for prayer requests, an event calendar, and access to sermon recordings.',
     
     technologies: ['Dart', 'Flutter', 'Mysql', 'mobile-app'],
-    image:'../assets/spritual tracer.jpg',
+    image:'/assets/spritual tracer.jpg',
     github: 'https://github.com/dagiteferi/Spiritual-Tracker',
     demo: '#'
   },
@@ -145,7 +145,7 @@ export const projects = [
     description: 'MoonLight Energy Solutions focuses on crafting a strategic approach to enhance operational efficiency and sustainability through targeted solar investments. ',
     
     technologies: ['Python' , 'Pandas', 'Numpy', 'Matplotlib', 'Seaborn', 'Streamlit'],
-    image:'../assets/solarfarm.png',
+    image:'/assets/solarfarm.png',
     github: 'https://github.com/dagiteferi/solar-farm-data_analysis',
     demo: 'https://moonlight-energy-ryl8arjoafnxkzwmendlb7.streamlit.app/'
   },
@@ -173,7 +173,7 @@ export const projects = [
     description: 'No description provided.',
     
     technologies: ['HTML' , 'CSS' , 'JavaScript', 'PHP', 'MySQL'],
-    image:'../assets/qr attendace.png',
+    image:'/assets/qr attendace.png',
     github: 'https://github.com/dagiteferi/Attendance-System',
     demo: 'https://attendance-system-plum-eta.vercel.app/'
   },
@@ -210,7 +210,7 @@ export const projects = [
     description: 'No description provided.',
     
     technologies: ['Html' , "css" , 'javascript'  ],
-    image:'../assets/anasimoseproject.jpeg',
+    image:'/assets/anasimoseproject.jpeg',
     github: 'https://github.com/dagiteferi/Anasimos',
     demo: 'https://attendance-system-plum-eta.vercel.app/'
   },
@@ -220,7 +220,7 @@ export const projects = [
     description: 'No description provided.',
     
     technologies: ['C++'],
-    image: '../assets/search sort.jpeg',
+    image: '/assets/search sort.jpeg',
     github: 'https://github.com/dagiteferi/Search-And-Sort-Algorithms-using-c-',
     demo: '#'
   },
@@ -230,7 +230,7 @@ export const projects = [
     description: 'the c++ program that store , update,delete data ,sort book by name ,ISBN number, it also store the data in database.txt file',
     
     technologies: ['C++'],
-    image:'../assets/lab managment.jpeg',
+    image:'/assets/lab managment.jpeg',
     github: 'https://github.com/dagiteferi/library-managment-sysem',
     demo: '#'
   },
