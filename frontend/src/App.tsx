d213810c61c4fcb089225ed7159af15f9507d3ab
dagi <dagiteferi2011@gmal.com>
2025-07-28 11:36:00 +0300

    feat: add AdminDashboard component and configure routing with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Pages from './components/pages/Pages';
 import SkeletonLoader from './components/Splash/SkeletonLoader';
+import AdminDashboard from './components/Admin/AdminDashboard';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate a loading delay
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); // Adjust the delay as needed
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="App">
-      {loading ? <SkeletonLoader /> : <Pages />}
+      {loading ? (
+        <SkeletonLoader />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/*" element={<Pages />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
